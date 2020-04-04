790ba3606c4a86c02f8ecb016207324c00be995b
Tran Thuy <dtranthuy1995@gmail.com>
2020-04-04 15:44:57 +0700

    Fix Two Sum
    Refactor Code Using

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -8,36 +8,29 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
-            int[] arra = new int[] { 2, 7, 11, 15 };
-            int[] pairIndicesNumber = FindIndicesTwoNumber(arra, 9);
+            //int[] arra = new int[] { 230, 863, 916, 585, 981, 404, 316, 785, 88, 12, 70, 435, 384, 778, 887, 755, 740, 337, 86, 92, 325, 422, 815, 650, 920, 125, 277, 336, 221, 847, 168, 23, 677, 61, 400, 136, 874, 363, 394, 199, 863, 997, 794, 587, 124, 321, 212, 957, 764, 173, 314, 422, 927, 783, 930, 282, 306, 506, 44, 926, 691, 568, 68, 730, 933, 737, 531, 180, 414, 751, 28, 546, 60, 371, 493, 370, 527, 387, 43, 541, 13, 457, 328, 227, 652, 365, 430, 803, 59, 858, 538, 427, 583, 368, 375, 173, 809, 896, 370, 789};
+            int[] arra = new int[] { 3, 2, 4 };
+            int[] pairIndicesNumber = TwoSum(arra, 6);
             for(int i = 0;i < pairIndicesNumber.Length;i++)
             {
                 Console.WriteLine(pairIndicesNumber[i]);
             }
         }
 
-        private static int[] FindIndicesTwoNumber(int[] arrays, int sum)        
+        public static int[] TwoSum(int[] nums, int target)
         {
             Dictionary<int, int> map = new Dictionary<int, int>();
-            int[] arraIndices = new int[2];
-            for(int i = 0;i < arrays.Length; i++)
+            for(int i = 0; i<nums.Length; i++)
             {
-                map.Add(arrays[i], i);
-            }
-
-
-            for(int i = 0;i < arrays.Length; i++)
-            {
-                var numberToFind = sum - arrays[i];
-                if (map.ContainsKey(numberToFind))
+                int value = target - nums[i];
+                if(map.ContainsKey(value) && map[value] != i)
                 {
-                    arraIndices[0] = i;
-                    arraIndices[1] = map[numberToFind];
-                    break;
+                    return new int[] { map[value], i };
                 }
+                map[nums[i]] = i;
             }
 
-            return arraIndices;
+            return null;
         }
     }
 }
